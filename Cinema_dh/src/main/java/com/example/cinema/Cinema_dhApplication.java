280161b0943f35ec7006a9911d0c214945a3721e
jun <lee@naver.com>
2022-09-15 10:24:25 +0900

    commit

--- Cinema_dh/src/main/java/com/example/cinema/Cinema_dhApplication.java
+++ Cinema_dh/src/main/java/com/example/cinema/Cinema_dhApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cinema1Application {
+public class Cinema_dhApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cinema1Application.class, args);
+		SpringApplication.run(Cinema_dhApplication.class, args);
 	}
 
 }
--- Cinema_dh/src/main/java/com/example/cinema/ServletInitializer.java
+++ Cinema_dh/src/main/java/com/example/cinema/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Cinema1Application.class);
+		return application.sources(Cinema_dhApplication.class);
 	}
 
 }
